242393409b6be35f6216f16f16e44ddfd99023c6
albertoCC05 <alberto.cst05@gmail.com>
2023-10-30 08:44:52 +0100

    intentando que el  mando de las narices funcione

---
+++ Assets/scripts/MovmentController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovmentController : MonoBehaviour
+{
+    private void Update()
+    {
+        // if (Input.GetAxis("Acelerate"))
+
+        Debug.Log(Input.GetButton("Acelerate"));
+    }
+}
