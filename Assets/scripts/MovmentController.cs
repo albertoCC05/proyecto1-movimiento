d1a72d018a11237901640282740e3829d7ac11f1
Alberto <alberto.cst05@gmail.com>
2023-11-01 22:16:03 +0100

    el mando funciona

--- Assets/scripts/MovmentController.cs
+++ Assets/scripts/MovmentController.cs
@@ -4,10 +4,47 @@ using UnityEngine;
 
 public class MovmentController : MonoBehaviour
 {
+
+    //Variables
+
+    [SerializeField] private float forwardSpeed;
+    [SerializeField] private float rotateSpeed;
+    private float verticalInput;
+    private float verticalNegativeInput;
+    private float horizontalInput;
+    
+
+
+
     private void Update()
     {
         // if (Input.GetAxis("Acelerate"))
 
         Debug.Log(Input.GetAxis("Acelerate"));
+        Debug.Log(Input.GetAxis("Frenar"));
+
+        verticalInput = Input.GetAxis("Acelerate");
+        verticalNegativeInput = Input.GetAxis("Frenar");
+        horizontalInput = Input.GetAxis("Rotate");
+
+        if (verticalInput<0)
+        {
+            verticalInput = 0;
+        }
+
+        if (verticalNegativeInput > 0)
+        {
+            verticalNegativeInput= 0;
+        }
+
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed * verticalNegativeInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed * verticalInput);
+        
+
+        if (verticalInput > 0 || verticalNegativeInput < 0)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed * horizontalInput);
+        }
+
     }
 }
